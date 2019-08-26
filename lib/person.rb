458daa304abc4bde153f11cd03a3d276395272f8
Katie Seggelke <katie.seggelke@gmail.com>
2019-08-26 20:10:39 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,12 +1,12 @@
 class Person
   def name 
-    @name = name 
+    @name
   end
   def name=(new_name)
     @name = new_name
   end 
   def job
-    @job = job 
+    @job 
   end 
   def job=(person_job)
     @job = person_job
